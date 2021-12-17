46bbb45118246b99baff6115203964945e9bd2d6
ext.shenxu1 <ext.shenxu1@jd.com>
2021-12-17 18:32:52 +0800

    ✨ feat:一版promise编写

---
+++ js-max/promise/index-min.js
@@ -0,0 +1,56 @@
+// 手写promise 版本1
+function MyPromise(fn) {
+  var self = this; // 缓存Promise实列
+  self.value = null; // Promise成功的值
+  self.error = null; // Promise失败的值
+
+  self.onResolvedCallback = null; // Promise的 resolve回调
+  self.onRejectedCallback = null; // Promise的 reject回调
+
+  // resovle回调处理
+  function resolve(value){
+    setTimeout(function(){
+      console.log(self.onResolvedCallback.toString());
+      self.value = value;
+      self.onResolvedCallback && self.onResolvedCallback(value);
+      self.onResolvedCallback(value);
+    }, 0);
+  }
+
+  // reject回调处理
+  function reject(error){
+    setTimeout(function(){
+      console.log('执行了reject')
+      self.error = error;
+      self.onRejectedCallback && self.onRejectedCallback(error);
+      (function(){
+        console.log('为啥不执行呢')
+      })()
+    }, 0);
+  }
+
+  fn(resolve, reject)
+}
+
+MyPromise.prototype.then = function(resolve, reject){
+  console.log('原型方法')
+  this.onResolvedCallback = resolve;
+  this.onRejectedCallback = reject;
+};
+
+const my = new MyPromise(function(resolve, reject){
+  const data = process.argv;
+  if(data && data[0]){
+    resolve(data[0]);
+  }else {
+    reject('error');
+  }
+})
+
+my.then(res => {
+  console.log(res);
+}, err => {
+  console.log(err);
+});
+
+
--- js-max/promise/index.js
+++ js-max/promise/index.js
@@ -1,6 +1,13 @@
-// 手写promise 版本1
+// 手写promise 加状态
+const PENDING = 'pending'; // 等待
+const FULFILLED = 'fulfilled'; // 完成
+const REJECTED = 'rejected'; // 失败
 
-function MyPromise(callback) {
-  var _this = this;
-  _this.value = void 0;
+function MyPromise(fn){
+  let self = this;
+  self.value = null; // Promise成功的值
+  self.error = null; // Promise失败的值
+
+  self.onResolvedCallback = null; // Promise的 resolve回调
+  self.onRejectedCallback = null; // Promise的 reject回调
 }
