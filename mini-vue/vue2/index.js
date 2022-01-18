32ecc8020e6937953ea24b4242397f9fa11adc5a
ext.shenxu5 <ext.shenxu5@jd.com>
2022-01-18 14:59:49 +0800

    ✨ feat:mini-vue2完善

--- mini-vue/vue2/index.js
+++ mini-vue/vue2/index.js
@@ -1,44 +1,156 @@
-import { _isNaN } from './util';
-import Observer from './observer';
-import Compiler from './compiler';
-
+function __isNaN(a,b){
+  return Number.isNaN(a) && Number.isNaN(b);
+}
 class miniVue{
-	constructor(options = {}) {
-		//在miniVue构造函数的内部
-		//保存根元素，能简便就尽量简便，不考虑数组情况
-		this.$el = typeof options.el === 'string' ? document.querySelector(options.el) : options.el;
-		this.$methods = options.methods;
-		this.$data = options.data;
-		this.$options = options;
-		
-		// 监听data数据
-		this.proxy(this.$data);
+  constructor(options = {}){
+      this.$options = options;
+      this.$el = typeof options.el === 'string' ? document.querySelector(options.el) : options.el;
+      this.$data = options.data;
+      this.$methods = options.methods;
+      this.proxy(this.$data);
+      // observer,拦截this.$data
+      new Observer(this.$data);
+      new Compiler(this);
+  }
+  // 代理,this.$data.xxx -> this.xxx
+  proxy(data){
+      Object.keys(data).forEach(key => {
+          Object.defineProperty(this,key,{
+              enumerable:true,
+              configurable:true,
+              get(){
+                  return data[key];
+              },
+              set(newValue){
+                  // NaN !== NaN
+                  if(data[key] === newValue || __isNaN(data[key],newValue))return;
+                  data[key] = newValue;
+              }
+          })
+      })
+  }
+}
+class Dep {
+  constructor(){
+      this.deps = new Set();
+  }
+  add(dep){
+      if(dep && dep.update)this.deps.add(dep);
+  }
+  notify(){
+      this.deps.forEach(dep => dep.update());
+  }
+}
+class Watcher {
+  constructor(vm,key,cb){
+      this.vm = vm;
+      this.key = key;
+      this.cb = cb;
+      Dep.target = this;
 
-		// 在miniVue构造函数内部
-		new Observer(this.$data);
+      this.__old = vm[key];
+      Dep.target = null;
+  }
+  update(){
+      let newValue = this.vm[this.key];
+      if(this.__old === newValue || __isNaN(this.__old,newValue))return;
+      this.cb(newValue);
+      this.__old = newValue;
+  }
+}
+// 不考虑数组
+class Observer {
+  constructor(data){
+      this.walk(data);
+  }
+  walk(data){
+      if(!data || typeof data !== 'object')return;
+      Object.keys(data).forEach(key => this.defineReactive(data,key,data[key]));
+  }
+  defineReactive(obj,key,value){
+      let vm = this;
+      this.walk(value);
+      let dep = new Dep();
+      Object.defineProperty(obj,key,{
+          configurable:true,
+          enumerable:true,
+          get(){
+              Dep.target && dep.add(Dep.target);
+              return value;
+          },
+          set(newValue){
+              if(value === newValue || __isNaN(value,newValue))return;
+              value = newValue;
+              vm.walk(newValue);
+              dep.notify();
+          }
+      })
+  }
+}
+class Compiler {
+  constructor(vm){
+      this.el = vm.$el;
+      this.vm = vm;
+      this.methods = vm.$methods;
+      this.compile(vm.$el);
+  }
 
-		// 编译类
-		new Compiler(this);
-	}
-
-	// 代理数据
-	//this.$data.xxx -> this.xxx;
-	//proxy代理实例上的data对象
-	proxy(data){
-		// 因为我们是代理每一个属性，所以我们需要将所有属性拿到
-		Object.keys(data).forEach(key => {
-			Object.defineProperty(this,key,{
-					enumerable:true,
-					configurable:true,
-					get:() => {
-							return data[key];
-					},
-					set:(newValue) => {
-							//这里我们需要判断一下如果值没有做改变就不用赋值，需要排除NaN的情况
-							if(newValue === data[key] || _isNaN(newValue,data[key])) return;
-							data[key] = newValue;
-					}
-			})
-		})
-	}
+  compile(el){
+      let childNodes = el.childNodes;
+      Array.from(childNodes).forEach(node => {
+          if(this.isTextNode(node)){
+              this.compileText(node);
+          }else if(this.isElementNode(node)){
+              this.compileElement(node);
+          }
+          if(node.childNodes && node.childNodes.length)this.compile(node)
+      })
+  }
+  compileElement(node){
+      if(node.attributes.length){
+          Array.from(node.attributes).forEach(attr => {
+              let attrName = attr.name;
+              if(this.isDirective(attrName)){
+                  attrName = attrName.indexOf(":") > -1 ? attrName.substr(5) : attrName.substr(2);
+                  let key = attr.value;
+                  this.update(node,key,attrName,this.vm[key]);
+              }
+          })
+      }
+  }
+  update(node,key,attrName,value){
+      if(attrName === 'text'){
+          node.textContent = value;
+          new Watcher(this.vm,key,val => node.textContent = val);
+      }else if(attrName === 'model'){
+          node.value = value;
+          new Watcher(this.vm,key,val => node.value = val);
+          node.addEventListener('input',() => {
+              this.vm[key] = node.value;
+          })
+      }else if(attrName === 'click'){
+          node.addEventListener(attrName,this.methods[key].bind(this.vm));
+      }
+  }
+  //{{ count }}
+  compileText(node){
+      let reg = /\{\{(.+?)\}\}/
+      let value = node.textContent;
+      if(reg.test(value)){
+          let key = RegExp.$1.trim();
+          node.textContent = value.replace(reg,this.vm[key]);
+          new Watcher(this.vm,key,val => {
+              node.textContent = val;
+          })
+      }
+  }
+  isDirective(dir){
+      return dir.startsWith('v-');
+  }
+  isTextNode(node){
+      return node.nodeType === 3;
+  }
+  isElementNode(node){
+      return node.nodeType === 1;
+  }
 }
