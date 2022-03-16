0557d6f5833a86e23d7ba01f1745d48d16dea5cc
ext.shenxu5 <ext.shenxu5@jd.com>
2022-03-16 23:20:41 +0800

    ✨ feat:新增版本

--- js-max/防抖节流/index.js
+++ js-max/防抖节流/index.js
@@ -10,6 +10,25 @@ var debounce = function(fn, delayTime) {
   }
 }
 
+// 立即执行版
+function debounce(func, wait) {
+  let timer;
+  return function() {
+    let context = this; // 这边的 this 指向谁?
+    let args = arguments; // arguments中存着e
+
+    if (timer) clearTimeout(timer);
+
+    let callNow = !timer;
+
+    timer = setTimeout(() => {
+      timer = null;
+    }, wait)
+
+    if (callNow) func.apply(context, args);
+  }
+}
+
 // 节流
 var throttle = function(fn, delayTime) {
   var _start = Date.now();
@@ -18,6 +37,21 @@ var throttle = function(fn, delayTime) {
     if(_now - _start >= delayTime) {
       fn.apply(context, args);
       _start = Date.now();
+    }
+  }
+}
+
+// 定时器版
+function throttle(func, wait) {
+  let timeout;
+  return function() {
+    let context = this;
+    let args = arguments;
+    if (!timeout) {
+      timeout = setTimeout(() => {
+        timeout = null;
+        func.apply(context, args)
+      }, wait)
     }
   }
 }
